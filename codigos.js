6917b81cf33af625e112e8368db94ccb2d0d02a3
Guilherme Abreu <guilherme.abreu378@gmail.com>
2025-04-25 21:44:48 -0300

    Add rodadas + Mudar timer

---
+++ codigos.js
@@ -0,0 +1,12 @@
+function getCodigos(){
+    let codigos = {
+        "rodada1": 3701,
+        "rodada2": 5263,
+        "rodada3": 7523,
+        "rodada4": 6325,
+        "rodada5": 8569,
+        "rodada6": 3067
+    }
+
+    return codigos;
+}
